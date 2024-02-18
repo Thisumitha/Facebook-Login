c7006c579f5b2bdab29858c6b14cb8d895d1ace7
Thisumitha <thisu2006@gmail.com>
2024-02-18 09:54:20 +0530

    Header component

---
+++ src/commponent/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+function Header() {
+  return (
+    <>
+      <div className="setlay">
+      <div className="">
+        <img className="fbLogo " src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" alt="Facebook" />
+      </div>
+      <h2 className="sidefont mt-4">Facebook helps you connect and share with the people in your life.</h2>
+    </div>
+    </>
+  );
+}
+
+export default Header;
